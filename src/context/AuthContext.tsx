ddb0bc2337a68c1a47462e2404cae90719dbca4d
IllangasingheIMDP <157776370+IllangasingheIMDP@users.noreply.github.com>
2025-01-31 13:23:32 +0530

    protected routes
    tb continued

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -23,10 +23,10 @@ export const AuthProvider=({children}:{children:ReactNode})=>{
         </AuthContext.Provider>
     );
 };
-export const useAuth=()=> {
-    const context=useContext(AuthContext);
-    if(!context){
-        throw new Error("useAuth must be used withing Authprovider");
-        return context;
-    }
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
